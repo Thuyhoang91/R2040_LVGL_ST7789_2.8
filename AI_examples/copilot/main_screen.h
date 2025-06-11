4eb6a591b460d6e1121ff586f0bcf1a667f69f29
Thuyhoang91 <transitruongthanh@gmail.com>
2025-06-11 07:53:44 +0700

    Create main_screen.h

---
+++ AI_examples/copilot/main_screen.h
@@ -0,0 +1,13 @@
+# ifndef MAIN_SCREEN_H
+# define MAIN_SCREEN_H
+
+//-------------------------------------
+// STATIC VARIABLES (File-Scope)    
+//-------------------------------------
+
+void create_main_screen(void);
+//void create_jog_screen(void);
+
+//-------------------------------------
+
+#endif
