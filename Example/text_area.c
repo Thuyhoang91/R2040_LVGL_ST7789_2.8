568799b1f437ea6d5b483fb2f58258521dde32cd
Thuyhoang91 <transitruongthanh@gmail.com>
2025-09-12 09:42:28 +0700

    Update text_area.c

--- Example/text_area.c
+++ Example/text_area.c
@@ -14,6 +14,8 @@ static void create_styled_textarea_cursor(const char * txt, lv_coord_t y_ofs, lv
     lv_obj_add_state(ta, LV_STATE_FOCUSED);
     lv_obj_add_style(ta, cursor_style, LV_PART_CURSOR | LV_STATE_FOCUSED);
     lv_textarea_set_cursor_pos(ta, 0);
+    lv_group_add_obj(group, ta);
+
 }
 
 void lv_textarea(void)
@@ -52,10 +54,11 @@ void lv_textarea(void)
 
     
     // Add the textareas to the group (the order will be their focus order)
-    lv_group_add_obj(group, lv_obj_get_child(lv_screen_active(), 0));
-    lv_group_add_obj(group, lv_obj_get_child(lv_screen_active(), 1));   
-    lv_group_add_obj(group, lv_obj_get_child(lv_screen_active(), 2));
+    //lv_group_add_obj(group, lv_obj_get_child(lv_screen_active(), 0));
+    //lv_group_add_obj(group, lv_obj_get_child(lv_screen_active(), 1));   
+    //lv_group_add_obj(group, lv_obj_get_child(lv_screen_active(), 2));
 }
 
 
 //#endif
+
